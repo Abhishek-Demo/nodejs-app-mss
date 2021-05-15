693e933821d51d6788862f3f47acd686f2ffa1a5
Abhishek8956 <75350806+Abhishek8956@users.noreply.github.com>
2021-05-15 10:39:52 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,8 +6,9 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.login': '348ea8a84cf89fd654284d70f0a8e82ccb518f5b'
+	   // 'sonar.login': 'admin',
+	  //  'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
